18848ccb1fa51df821335ed76e7b9f4e974527d0
Satoru Nakamura <na.kamura.1263@gmail.com>
2024-07-07 14:19:40 +0900

    feat: udpate demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,6 +4,19 @@ import annotationPlugins from '../../src';
 import LocalStorageAdapter from '../../src/LocalStorageAdapter';
 import AnnototAdapter from '../../src/AnnototAdapter';
 
+var params = new URL(document.location).searchParams;
+var manifest = params.get('iiif-content') || params.get('manifest') || 'https://iiif.harvardartmuseums.org/manifests/object/299843'
+var windows = [];
+if (manifest) {
+  windows.push({ manifestId: manifest });
+}
+/*
+var miradorInstance = Mirador.viewer({
+  id: 'mirador',
+  windows: windows,
+});
+*/
+
 const endpointUrl = 'http://127.0.0.1:3000/annotations';
 const config = {
   annotation: {
@@ -16,9 +29,9 @@ const config = {
     defaultSideBarPanel: 'annotations',
     sideBarOpenByDefault: true,
   },
-  windows: [{
+  windows /*: [{
     loadedManifest: 'https://iiif.harvardartmuseums.org/manifests/object/299843',
-  }],
+  }]*/,
 };
 
 mirador.viewer(config, [...annotationPlugins]);
